97ee357d3263cc15cf2265f7d77498ff5585e7ba
miluku1018 <miluku1018@gmail.com>
2020-03-02 02:12:30 +0800

    add vote2

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,12 +1,18 @@
 class VotesController < ApplicationController
   def create
-    post_id = params[:vote][:post_id]
-    vote = Vote.new(vote_params)
+    post_id = params[:post_id]
+    
+    vote = Vote.new
+    vote.post_id = params[:post_id]
+    vote.upvote = params[:upvote]
     vote.account_id = current_account.id
+    
     
     #check if vote by this user exists
     existing_vote = Vote.where(account_id: current_account.id, post_id: post_id)
-    respon_to do |format|
+    @new_vote = existing_vote.size < 1
+
+    respond_to do |format|
       format.js {
         if existing_vote.size > 0
           #destroy existing vote
@@ -18,11 +24,10 @@ class VotesController < ApplicationController
           else
             @success = false
           end  
-
-          @post = Post.find(post_id)
-          @total_upvotes = @psot.upvotes 
-          @total_downvotes = @psot.downvotes
         end
+
+        @post = Post.find(post_id)
+        @is_upvote = params[:upvote]
         render "votes/create" 
       }
     end
